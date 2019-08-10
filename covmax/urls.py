5884dc0cba09df2d99db417fc0793c3fe8159723
Kuan <kuanjh123@gmail.com>
2019-08-10 09:12:38 -0500

    Revert "Revert "User interface update""
    This reverts commit cb89758ea93792cc9041f3ad6f664d0eaefabd49.

--- covmax/urls.py
+++ covmax/urls.py
@@ -14,12 +14,28 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.conf import settings
 from covmax.constants import timer_working, timer_stop 
 from django.conf.urls import url, include
 from game.models import start_timer
-from game.views import game_view, board_view, main_view, admin_view, admin_observation_view, graph_view, end_round_view, attach0_view, end_view, download
+from game.views import game_view, board_view, main_view, admin_view, admin_observation_view, graph_view, end_round_view, attach0_view, end_view, download, post_survey_view
+from survey.views import ConfirmView, IndexView, SurveyCompleted, SurveyDetail
+
+'''
+urlpatterns = [
+    url(r"^$", IndexView.as_view(), name="survey-list"),
+    url(r"^(?P<id>\d+)/", SurveyDetail.as_view(), name="survey-detail"),
+    url(r"^csv/(?P<primary_key>\d+)/", serve_result_csv, name="survey-result"),
+    url(r"^(?P<id>\d+)/completed/", SurveyCompleted.as_view(), name="survey-completed"),
+    url(
+        r"^(?P<id>\d+)-(?P<step>\d+)/",
+        SurveyDetail.as_view(),
+        name="survey-detail-step",
+    ),
+    url(r"^confirm/(?P<uuid>\w+)/", ConfirmView.as_view(), name="survey-confirmation"),
+]
+'''
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main_view, name='home'),
@@ -32,12 +48,13 @@ urlpatterns = [
     path('end', end_view, name='end'),
     path('attach0', attach0_view, name='home'),
     path('download', download, name='download'),
-    url(r'^survey/', include('survey.urls'))
-    
+    re_path(r"^survey/(?P<id>\d+)/", SurveyDetail.as_view(), name="survey-detail"),
+    re_path(r"^survey/confirm/(?P<uuid>\w+)/", post_survey_view, name="survey-confirmation"),
+
     
 ]
 
-start_timer(timer_working, timer_stop)
+#start_timer(timer_working, timer_stop)
 '''
 
 '''
