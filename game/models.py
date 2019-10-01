0905a7a56b71f3ea6c906b8b677d8b614fd858ff
Kuan <kuanjh123@gmail.com>
2019-10-01 16:52:19 -0500

    Update models.py

--- game/models.py
+++ game/models.py
@@ -754,13 +754,13 @@ class Config(models.Model):
     def generate_game(self,player_id):
         # Progress ranges from 0 to 3, initialized to -1
 
-        real_id = player_id - Player.objects.first().id 
+        real_id = str(player_id - Player.objects.first().id)
         if real_id not in self.parsed_assigner['table']:
             tab =  self.parsed_assigner['table']
-            assert(real_id % 6 == 0)
+            assert(int(real_id) % 6 == 0)
             for di in self.assigner_table:
-                self.parsed_assigner['table'][real_id + di] = copy(self.assigner_table[di])
-                self.parsed_assigner['progress'][real_id + di] = -1
+                self.parsed_assigner['table'][int(real_id) + di] = copy(self.assigner_table[di])
+                self.parsed_assigner['progress'][int(real_id) + di] = -1
         self.parsed_assigner['progress'][real_id] += 1 
         # Update 
         # Find current progress 
@@ -784,7 +784,7 @@ class Config(models.Model):
                 print(msg)
                 # Update everyone else 
                 for i in range(6):
-                    peer_id = (real_id // 6) * 6 + i
+                    peer_id = (int(real_id) // 6) * 6 + i
                     for j, cell in enumerate(self.parsed_assigner['table'][peer_id]):
                         if cell == -ind:
                             self.parsed_assigner['table'][peer_id][j] = new_game.id 
