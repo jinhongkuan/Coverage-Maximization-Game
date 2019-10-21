1cd214b4b281f3d9e9859153fe4094010a49ef16
Kuan <kuanjh123@gmail.com>
2019-10-20 19:51:55 -0500

    Update settings.py

--- covmax/settings.py
+++ covmax/settings.py
@@ -90,15 +90,25 @@ WSGI_APPLICATION = 'covmax.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'database-1',
-        'USER': 'admin',
-        'PASSWORD': '12345678'
 
+if 'RDS_HOSTNAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'dbp.sqlite3'),
+        }
+    }
 
 
 # Password validation
