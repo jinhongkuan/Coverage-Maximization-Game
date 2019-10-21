bac1dd548b7a67f0649a4323e9c949279e458608
Kuan <kuanjh123@gmail.com>
2019-10-21 18:51:11 -0500

    new

--- clear_all.py
+++ clear_all.py
@@ -1,16 +1,19 @@
-from game.models import Game, Board, Config
+from game.models import Game, Board, Config, Sequence
 from player.models import Player, Agent 
 # Bug fix
 Game.objects.all().delete()
 Board.objects.all().delete()
 Player.objects.all().delete()
 Agent.objects.all().delete()
+Sequence.objects.all().delete()
 
 if len(Config.objects.filter(main=True)) == 0:
     Config.objects.create(main=True)
 
 A = Config.objects.get(main=True)
 A.assigner = '{"table":{},"progress":{}}'
+A.generate_seq("exp3",[("Map 1.csv",200),("Map 2.csv",200),("Map 3.csv",200),("Map 4.csv",200)], "BLLL(instance,2,0.25)")
 A.timer_enabled = True 
 A.snapshot_interval = 1 
 A.save()
+
