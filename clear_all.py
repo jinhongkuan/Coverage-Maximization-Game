25c6e0b049bf2d9776332874ebdcdfb596008efe
Kuan <kuanjh123@gmail.com>
2019-10-20 23:21:44 -0500

    Update clear_all.py

--- clear_all.py
+++ clear_all.py
@@ -5,6 +5,10 @@ Game.objects.all().delete()
 Board.objects.all().delete()
 Player.objects.all().delete()
 Agent.objects.all().delete()
+
+if len(Config.objects.get(main=True)) == 0:
+    Config.objects.create(main=True)
+
 A = Config.objects.get(main=True)
 A.assigner = '{"table":{},"progress":{}}'
 A.timer_enabled = True 
