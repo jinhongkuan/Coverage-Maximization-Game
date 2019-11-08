dd85377c9095679df2f4c3a133bb890b328f15e2
Kuan <kuanjh123@gmail.com>
2019-11-07 21:12:14 -0600

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -17,6 +17,7 @@ for game in Game.objects.all():
             else:
                 x = survey.player_id
             board = Board.objects.get(id=game.board_id)
+            print(str(board))
             output += [[game.id, x, len(game.parsed_human_players), board.name, board.getScore(),board.getOptimalScore(), ",".join(board.parsed_score_history), survey.difficulty, survey.satisfaction, survey.confusion, survey.collaboration, survey.contribution, survey.interaction, survey.isolation, survey.activity, survey.understanding, survey.intelligence ]]
 
 with open("data.csv", "w", newline="") as f:
